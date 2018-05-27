9a5689d8920ba5c3e80032df64523d97d0fb0401
Daniil Basmanov <basmanovdaniil@gmail.com>
2018-05-27 18:43:27 +0300

    Geometry2DLineRayTest: Added ClosestPoints tests

--- Editor/Geometry2DLineRayTest.cs
+++ Editor/Geometry2DLineRayTest.cs
@@ -5,6 +5,95 @@ namespace ProceduralToolkit.Tests
 {
     public class Geometry2DLineRayTest : GeometryTest
     {
+        #region ClosestPoints
+
+        [Test]
+        public void ClosestPoints_Collinear()
+        {
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 direction = Vector2.up.RotateCW(i).normalized;
+                var line = new Line2(Vector2.zero, direction);
+
+                ClosestPoints_Collinear(line, Vector2.zero, direction);
+                ClosestPoints_Collinear(line, direction, direction);
+                ClosestPoints_Collinear(line, direction*100, direction);
+                ClosestPoints_Collinear(line, -direction, direction);
+                ClosestPoints_Collinear(line, -direction*100, direction);
+            }
+        }
+
+        private void ClosestPoints_Collinear(Line2 line, Vector2 origin, Vector2 direction)
+        {
+            Vector2 linePoint;
+            Vector2 rayPoint;
+            Geometry.ClosestPointsLineRay(line, new Ray2D(origin, direction), out linePoint, out rayPoint);
+            AreEqual(linePoint, origin);
+            AreEqual(rayPoint, origin);
+            Geometry.ClosestPointsLineRay(line, new Ray2D(origin, -direction), out linePoint, out rayPoint);
+            AreEqual(linePoint, origin);
+            AreEqual(rayPoint, origin);
+        }
+
+        [Test]
+        public void ClosestPoints_Parallel()
+        {
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 direction = Vector2.up.RotateCW(i).normalized;
+                Vector2 perpendicular = direction.RotateCW(90);
+                var line = new Line2(Vector2.zero, direction);
+                ClosestPoints_Parallel(line, perpendicular, direction, Vector2.zero);
+                ClosestPoints_Parallel(line, perpendicular + direction*50, direction, direction*50);
+                ClosestPoints_Parallel(line, perpendicular - direction*50, direction, -direction*50);
+                ClosestPoints_Parallel(line, perpendicular*50, direction, Vector2.zero);
+                ClosestPoints_Parallel(line, perpendicular*50 + direction*20, direction, direction*20);
+                ClosestPoints_Parallel(line, perpendicular*50 - direction*20, direction, -direction*20);
+            }
+        }
+
+        private void ClosestPoints_Parallel(Line2 line, Vector2 origin, Vector2 direction, Vector2 expectedLinePoint)
+        {
+            Vector2 linePoint;
+            Vector2 rayPoint;
+            Geometry.ClosestPointsLineRay(line, new Ray2D(origin, direction), out linePoint, out rayPoint);
+            AreEqual(linePoint, expectedLinePoint);
+            AreEqual(rayPoint, origin);
+            Geometry.ClosestPointsLineRay(line, new Ray2D(origin, -direction), out linePoint, out rayPoint);
+            AreEqual(linePoint, expectedLinePoint);
+            AreEqual(rayPoint, origin);
+        }
+
+        [Test]
+        public void ClosestPoints_Perpendicular()
+        {
+            for (int i = 0; i < 360; i++)
+            {
+                Vector2 direction = Vector2.up.RotateCW(i).normalized;
+                Vector2 perpendicular = direction.RotateCW(90);
+                var line = new Line2(Vector2.zero, direction);
+                ClosestPoints_Perpendicular(line, Vector2.zero, perpendicular, Vector2.zero);
+                ClosestPoints_Perpendicular(line, -perpendicular, perpendicular, Vector2.zero);
+                ClosestPoints_Perpendicular(line, -perpendicular*100, perpendicular, Vector2.zero);
+
+                ClosestPoints_Perpendicular(line, perpendicular, perpendicular, perpendicular);
+                ClosestPoints_Perpendicular(line, perpendicular*100, perpendicular, perpendicular*100);
+                ClosestPoints_Perpendicular(line, -perpendicular, -perpendicular, -perpendicular);
+                ClosestPoints_Perpendicular(line, -perpendicular*100, -perpendicular, -perpendicular*100);
+            }
+        }
+
+        private void ClosestPoints_Perpendicular(Line2 line, Vector2 origin, Vector2 direction, Vector2 expectedRayPoint)
+        {
+            Vector2 linePoint;
+            Vector2 rayPoint;
+            Geometry.ClosestPointsLineRay(line, new Ray2D(origin, direction), out linePoint, out rayPoint);
+            AreEqual(linePoint, line.origin);
+            AreEqual(rayPoint, expectedRayPoint);
+        }
+
+        #endregion ClosestPoints
+
         #region Intersect
 
         [Test]
